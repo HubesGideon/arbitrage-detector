4fa8a946fa351f3042bb5e889fab527fe16b8ec7
HubesGideon <aidengideon20@gmail.com>
2025-05-01 10:55:24 -0500

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -1,1 +1,9 @@
+import requests
+import os
+
+DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
+
+def notify_discord(message):
+    payload = {"content": message}
+    requests.post(DISCORD_WEBHOOK_URL, json=payload)
 
