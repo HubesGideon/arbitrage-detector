4672fe29f7223b05c10cdcaa50e32454d49e2203
HubesGideon <aidengideon20@gmail.com>
2025-05-01 11:27:24 -0500

    Update arbitrage.py

--- arbitrage.py
+++ arbitrage.py
@@ -1,34 +1,26 @@
 def detect_arbitrage(game):
     arbs = []
-    for market in game.get("bookmakers", []):
-        for sub_market in market.get("markets", []):
-            market_type = sub_market["key"]
-            outcomes = sub_market["outcomes"]
-            if len(outcomes) != 2:
-                continue
-            arbs += compare_with_other_books(game, market_type, sub_market, market["title"])
+    for book1 in game.get("bookmakers", []):
+        for market1 in book1.get("markets", []):
+            for book2 in game.get("bookmakers", []):
+                if book1["title"] == book2["title"]:
+                    continue
+                for market2 in book2.get("markets", []):
+                    if market1["key"] != market2["key"]:
+                        continue
+                    try:
+                        o1 = market1["outcomes"][0]["price"]
+                        o2 = market2["outcomes"][1]["price"]
+                        inv_sum = (1 / o1) + (1 / o2)
+                        profit_margin = (1 - inv_sum) * 100
+                        if profit_margin >= 2:
+                            arbs.append({
+                                "teams": game["teams"],
+                                "market": market1["key"],
+                                "bookmakers": (book1["title"], book2["title"]),
+                                "odds": (o1, o2),
+                                "profit_margin": profit_margin
+                            })
+                    except:
+                        continue
     return arbs
-
-def compare_with_other_books(game, market_type, base_market, base_bookmaker_name):
-    arbs_found = []
-    base_outcomes = base_market["outcomes"]
-    for book in game.get("bookmakers", []):
-        for market in book.get("markets", []):
-            if market["key"] != market_type:
-                continue
-            outcomes = market["outcomes"]
-            if len(outcomes) != 2:
-                continue
-            try:
-                inv_sum = (1 / base_outcomes[0]["price"]) + (1 / outcomes[1]["price"])
-                if inv_sum < 1:
-                    arbs_found.append({
-                        "teams": game["teams"],
-                        "market": market_type,
-                        "bookmakers": (base_bookmaker_name, book["title"]),
-                        "odds": (base_outcomes[0]["price"], outcomes[1]["price"]),
-                        "profit_margin": (1 - inv_sum) * 100
-                    })
-            except:
-                continue
-    return arbs_found
