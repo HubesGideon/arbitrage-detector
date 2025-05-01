f089cc353b6578b8274514e9f2f50da291588081
HubesGideon <aidengideon20@gmail.com>
2025-05-01 10:54:50 -0500

    Update odds_api.py

--- odds_api.py
+++ odds_api.py
@@ -1,1 +1,17 @@
+import requests
+import os
+
+API_KEY = os.getenv("ODDS_API_KEY")
+
+def fetch_odds():
+    url = "https://api.the-odds-api.com/v4/sports/baseball_mlb/odds/"
+    params = {
+        'apiKey': API_KEY,
+        'regions': 'us',
+        'markets': 'h2h',
+        'oddsFormat': 'decimal'
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.json()
 
