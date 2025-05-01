3b3ca163323dbe53275506574c118d82b67d2a27
HubesGideon <aidengideon20@gmail.com>
2025-05-01 11:02:39 -0500

    Update arbitrage.py

--- arbitrage.py
+++ arbitrage.py
@@ -1,19 +1,34 @@
 def detect_arbitrage(game):
-    books = game['bookmakers']
-    for i in range(len(books)):
-        for j in range(i + 1, len(books)):
+    arbs = []
+    for market in game.get("bookmakers", []):
+        for sub_market in market.get("markets", []):
+            market_type = sub_market["key"]
+            outcomes = sub_market["outcomes"]
+            if len(outcomes) != 2:
+                continue
+            arbs += compare_with_other_books(game, market_type, sub_market, market["title"])
+    return arbs
+
+def compare_with_other_books(game, market_type, base_market, base_bookmaker_name):
+    arbs_found = []
+    base_outcomes = base_market["outcomes"]
+    for book in game.get("bookmakers", []):
+        for market in book.get("markets", []):
+            if market["key"] != market_type:
+                continue
+            outcomes = market["outcomes"]
+            if len(outcomes) != 2:
+                continue
             try:
-                team1_odds = books[i]['markets'][0]['outcomes'][0]['price']
-                team2_odds = books[j]['markets'][0]['outcomes'][1]['price']
-                inv_sum = (1 / team1_odds) + (1 / team2_odds)
+                inv_sum = (1 / base_outcomes[0]["price"]) + (1 / outcomes[1]["price"])
                 if inv_sum < 1:
-                    return {
-                        "teams": game['teams'],
-                        "bookmakers": (books[i]['title'], books[j]['title']),
-                        "odds": (team1_odds, team2_odds),
+                    arbs_found.append({
+                        "teams": game["teams"],
+                        "market": market_type,
+                        "bookmakers": (base_bookmaker_name, book["title"]),
+                        "odds": (base_outcomes[0]["price"], outcomes[1]["price"]),
                         "profit_margin": (1 - inv_sum) * 100
-                    }
+                    })
             except:
                 continue
-    return None
-
+    return arbs_found
