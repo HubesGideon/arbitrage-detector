407cec2f9f52f0f62dbf7a29fd5e123a92fca7d7
HubesGideon <aidengideon20@gmail.com>
2025-05-04 13:47:36 -0500

    Update arbitrage.py

--- arbitrage.py
+++ arbitrage.py
@@ -13,19 +13,28 @@ def detect_arbitrage(game):
                     if market1["key"] != market2["key"]:
                         continue
                     try:
-                        if len(market1["outcomes"]) != 2 or len(market2["outcomes"]) != 2:
+                        # Ensure both markets have at least 2 outcomes
+                        if len(market1["outcomes"]) < 2 or len(market2["outcomes"]) < 2:
                             continue
-                        o1 = market1["outcomes"][0]["price"]
-                        o2 = market2["outcomes"][1]["price"]
+
+                        outcome1 = market1["outcomes"][0]
+                        outcome2 = market2["outcomes"][1]
+
+                        o1 = outcome1["price"]
+                        o2 = outcome2["price"]
+
                         inv_sum = (1 / o1) + (1 / o2)
                         profit_margin = (1 - inv_sum) * 100
+
                         if profit_margin >= 2:
                             arbs.append({
                                 "teams": teams,
                                 "market": market1["key"],
                                 "bookmakers": (book1["title"], book2["title"]),
                                 "odds": (o1, o2),
-                                "profit_margin": profit_margin
+                                "profit_margin": profit_margin,
+                                "outcome1": outcome1,
+                                "outcome2": outcome2
                             })
                     except:
                         continue
